5ced8e268e0327020f7053919e5de19c35333cc1
Christian <christian@sanglech.com>
2021-04-29 18:17:58 +0900

    Added Update Function
    -Added Update button
    - Controller code for update
    - add hidden field for id

--- src/main/java/com/christian/springbootthymeleafdemo/controller/EmployeeController.java
+++ src/main/java/com/christian/springbootthymeleafdemo/controller/EmployeeController.java
@@ -5,10 +5,7 @@ import com.christian.springbootthymeleafdemo.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.PostConstruct;
 import java.util.ArrayList;
@@ -46,6 +43,22 @@ public class EmployeeController {
 
         return "employees/employee-form";
     }
+
+    @GetMapping("/showFormForUpdate")
+    public String showFormForUpdate(@RequestParam("employeeId") int theId, Model theModel){
+        //get employee from the service
+        Employee theEmployee = employeeService.getEmployeeById(theId);
+
+        //prepopulate model
+
+        theModel.addAttribute("employee",theEmployee);
+
+        //send to our form page
+        return "employees/employee-form";
+    }
+
+
+
 
     @PostMapping("/save")
     public String saveEmployee(@ModelAttribute("employee") Employee theEmployee){
