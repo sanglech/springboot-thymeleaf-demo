cc612fc7de1a731617acb8cd287538e5d35c6919
Christian <christian@sanglech.com>
2021-04-29 17:08:14 +0900

    Refactored file locations  for employees

--- src/main/java/com/christian/springbootthymeleafdemo/controller/EmployeeController.java
+++ src/main/java/com/christian/springbootthymeleafdemo/controller/EmployeeController.java
@@ -32,6 +32,6 @@ public class EmployeeController {
 
         theModel.addAttribute("theEmployees",employees);
 
-        return "list-employees";
+        return "employees/list-employees";
     }
 }
